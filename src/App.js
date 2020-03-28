94c7499af4e8dfc460f7c75d589895eb81ad794a
Aliou S Maiga <badrasadja@gmail.com>
2020-03-28 01:39:53 -0700

    Removed logo.svg; changed css file; initiated state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import Person from './Person/Person'
+
 import './App.css';
+import './Person/Person.css'
 
 class App extends Component {
+  state = {
+    persons :  [
+      {name:'Aliou', age:'40', hobby:'travelling'},
+      {name:'Titi', age:'37',  hobby:'reading'}
+    ],
+    count: 0
+  }
+
+
+  togglePersonHandler = () => {
+
+    
+  }
+  switchNameHandler = () => {
+    setTimeout(() => {
+      this.setState({
+        persons: [
+        {name:'Damus', age:'41', hobby:'Music'},
+        {name:'Ina', age:'37',  hobby:'travelling'}
+      ]});
+    }, 1000)
+
+  }
+
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+      {name:'Damus', age:'41', hobby:'Music'},
+      {name: event.target.value, age:'37',  hobby:'travelling'}
+    ]});
+  }
+
+incrementCount = () => {
+ this.setState({
+   count: this.state.count + 1
+ });
+
+}
+ getInitialSatet = () => {
+   return {
+     count: 0
+   }
+ }
   render() {
+
+  const style = {
+   backgroundColor: 'White',
+   font: 'inherit',
+   border: '1px solid blue',
+   padding: '8px',
+   cursor: 'pointer'
+  };
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 >Hi, I am a React App</h1>
+          <button style={style} onClick={this.togglePersonHandler}>Switch Name</button>
+          <div id="person">
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+            click={this.switchNameHandler.bind(this, 'Alfordville')}
+            changed={this.nameChangeHandler}>
+            My hobbies: {this.state.persons[0].hobby}</Person>
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}>
+            My hobbies: {this.state.persons[1].hobby}</Person>
+          </div>
+
+            <button onClick={this.incrementCount}>{this.state.count}</button>
         </header>
       </div>
     );
